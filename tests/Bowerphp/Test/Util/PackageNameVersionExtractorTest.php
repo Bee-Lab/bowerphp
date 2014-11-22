6061502576328800145f4c42aa5e66f5c3aa9608
piotrooo <piotroo89@gmail.com>
2014-11-22 10:56:46 +0100

    Extract PackageNameVersionExtractor class.

---
+++ tests/Bowerphp/Test/Util/PackageNameVersionExtractorTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Bowerphp\Test\Util;
+
+use Bowerphp\Util\PackageNameVersionExtractor;
+use PHPUnit_Framework_TestCase;
+
+class PackageNameVersionExtractorTest extends PHPUnit_Framework_TestCase
+{
+    public function testReturnPackageNameWhenNoVersionSet()
+    {
+        //given
+        $package = 'jquery';
+
+        //when
+        $packageNameVersion = PackageNameVersionExtractor::fromString($package);
+
+        //then
+        $this->assertEquals('jquery', $packageNameVersion->name);
+        $this->assertEquals('*', $packageNameVersion->version);
+    }
+
+    public function testReturnPackageAndVersionIsSet()
+    {
+        //given
+        $package = 'jquery#1.10.2';
+
+        //when
+        $packageNameVersion = PackageNameVersionExtractor::fromString($package);
+
+        //then
+        $this->assertEquals('jquery', $packageNameVersion->name);
+        $this->assertEquals('1.10.2', $packageNameVersion->version);
+    }
+}
