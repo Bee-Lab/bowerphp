7bfcbd295279b919680ab3f77eadb261e1ad4c5a
mike <mike.simonson@gmail.com>
2015-01-02 15:08:39 +0100

    Trying to reduce a bit the complexity of the compile method. And using the package, version and date, variables.

--- src/Bowerphp/Bowerphp.php
+++ src/Bowerphp/Bowerphp.php
@@ -7,6 +7,9 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * Version @package_version@
+ * Released on the @release_date@
  */
 
 namespace Bowerphp;
