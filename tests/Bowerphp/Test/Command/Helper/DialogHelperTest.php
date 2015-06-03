f0f99ca6cbd9070f10ca2e3314e885ad38e9f9a3
nickdietel <nick.dietel@gmail.com>
2015-06-03 14:39:01 +0200

    Disabled triggerDeprecationError
    to avoid getting the DeprecationError trigger during test

--- tests/Bowerphp/Test/Command/Helper/DialogHelperTest.php
+++ tests/Bowerphp/Test/Command/Helper/DialogHelperTest.php
@@ -8,7 +8,7 @@ class DialogHelperTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetQuestion()
     {
-        $helper = new DialogHelper();
+        $helper = new DialogHelper(false);
         $this->assertEquals('<info>This is a question</info> [<comment>default reply</comment>]: ', $helper->getQuestion('This is a question', 'default reply'));
         $this->assertEquals('<info>Another question</info>- ', $helper->getQuestion('Another question', null, '-'));
     }
